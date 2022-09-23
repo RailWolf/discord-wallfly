9b1978a93fac5dd9e2927b45e88e7528caaaffbd
Brandon Calhoun <wailrofl@gmail.com>
2022-09-23 15:04:19 -0700

    Create bot_cfg.example.rb

---
+++ bot_cfg.example.rb
@@ -0,0 +1,7 @@
+# Copy to "bot_cfg.rb" and update token + paths if needed
+
+$discord_token = 'token'
+
+$server_status = "../server-status/server-status.rb"
+
+$server_info = '../dorkbuster/server-info.cfg'
